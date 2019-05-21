0e59ad0e9da779566454749719bca63eed9fcae2
isolin0 <46123824+isolin0@users.noreply.github.com>
2019-05-20 22:01:23 -0300

    ProyectDemo 00.00

---
+++ steamVR1.2/Assets/SCRIPTS/ViewManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewManager : MonoBehaviour
+{
+	public Camera cam;
+	public GameObject conDisp;
+
+	int standerMask;
+
+	private void Start()
+	{
+		standerMask = cam.cullingMask;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+
+		if (other.tag == "Devise")
+		{
+			cam.cullingMask = (1 << 0) | (1 << 8);
+			conDisp.SetActive(true);
+		}
+
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Devise")
+		{
+			cam.cullingMask = standerMask;
+			conDisp.SetActive(false);
+		}
+	}
+
+}
