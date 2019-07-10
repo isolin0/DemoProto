8274918d2530f1b1ed31c70f127cf04c1ba54cc3
isolin0 <46123824+isolin0@users.noreply.github.com>
2019-07-10 18:42:39 -0300

    10del7

---
+++ steamVR1.2/Assets/SCRIPTS----------/IgnoreCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Ignore the collisions between layer 0 (default) and layer 8 (custom layer you set in Inspector window)
+		Physics.IgnoreLayerCollision(0, 2);
+	}
+
+    
+}
